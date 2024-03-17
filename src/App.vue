61a09e6597075e19827812b3dc47696fccd35eb2
vaaski <admin@vaa.ski>
2024-03-17 20:27:45 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import VideoPlayer from "./components/VideoPlayer.vue"
+</script>
+
+<template>
+  <VideoPlayer
+    src="https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4"
+  />
+</template>
---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useMediaControls } from "@vueuse/core"
+import { ref } from "vue"
+
+const props = defineProps<{
+  src: string
+}>()
+
+const videoElement = ref<HTMLVideoElement>()
+
+const { playing, currentTime, duration } = useMediaControls(videoElement, {
+  src: props.src,
+})
+</script>
+
+<template>
+  <video ref="videoElement" controls></video>
+
+  <span>playing: {{ playing }} {{ currentTime }} {{ duration }}</span>
+</template>
